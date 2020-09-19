93d244623bc6a12cf068f716f547b51c405380d8
Matheus Maghost <matheus@gamersclub.com.br>
2020-09-18 22:22:49 -0300

    feat: add scrollbar controller to change scrollOffset value

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,19 +7,55 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  ScrollController _scrollController;
+
+  double _scrollOffset = 0.0;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController()
+      ..addListener(() {
+        setState(() {
+          _scrollOffset = _scrollController.offset;
+        });
+      });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.green[850],
+        backgroundColor: Colors.grey[850],
         child: const Icon(Icons.cast),
         onPressed: () => print('Cast'),
       ),
       appBar: PreferredSize(
         preferredSize: Size(screenSize.width, 50.0),
-        child: CustomAppBar(),
+        child: CustomAppBar(
+          scrollOffset: _scrollOffset,
+        ),
+      ),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              height: 1000.0,
+              color: Colors.blue,
+            ),
+          )
+        ],
       ),
     );
   }
