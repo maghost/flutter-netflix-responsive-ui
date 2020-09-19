f07874eb898719e279fa16d574f4a38ee09b5ba2
Matheus Maghost <matheus@gamersclub.com.br>
2020-09-18 21:59:25 -0300

    feat: create home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green[850],
+        child: const Icon(Icons.cast),
+        onPressed: () => print('Cast'),
+      ),
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 50.0),
+        child: Container(),
+      ),
+    );
+  }
+}
